8cd7b158726c84737afaaf29e39981a3e942c5f6
Alex Tetervak <tetervak@sheridancollege.ca>
2019-04-12 17:43:17 -0400

    migrated to androidx

--- app/src/main/java/ca/javateacher/catmessage1/OutputActivity.java
+++ app/src/main/java/ca/javateacher/catmessage1/OutputActivity.java
@@ -1,7 +1,7 @@
 /* Alex Tetervak, Sheridan College, Ontario */
 package ca.javateacher.catmessage1;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
